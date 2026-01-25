2f099fb8c4731a0c23d9d6fa32767144a92a2505
SrPhilippe <philippecoding@outlook.com>
2026-01-25 12:25:03 -0300

    update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors())
+app.use(cors({
+    origin: ['https://cirurtec.com.br', 'http://localhost:5173'] // Allow requests from this origin
+}))
 app.use(express.json())
 
 // Routes
